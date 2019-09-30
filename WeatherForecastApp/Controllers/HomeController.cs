2d4ab2cea5a7e569bb04644d5efe226466c43ee3
Dimych <dimayovko@gmail.com>
2019-09-30 18:58:38 +0300

    Adding project

---
+++ WeatherForecastApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+using WeatherForecastApp.Models;
+
+namespace WeatherForecastApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        ProjectModel Model = new ProjectModel();
+
+        public HomeController()
+        {
+            BuildUrl();//вызываем метод, для постройки Url
+        }
+
+        public ActionResult Weather()
+        {
+
+            return View();
+        }
+        //методы, для изменения части строки
+        public string ReturnNewPathForecast()
+        {
+            Model.UriBuilder.Path = "forecast";
+
+            return Model.UriBuilder.ToString();
+        }
+
+        public string ReturnNewPathWeather()
+        {
+            Model.UriBuilder.Path = "weather";
+
+            return Model.UriBuilder.ToString();
+        }
+
+        
+
+        private void BuildUrl()
+        {
+            Model.UriBuilder.Scheme = "https";
+            Model.UriBuilder.Host = "api.openweathermap.org/data/2.5";
+            Model.UriBuilder.Query = $"units=metric&appid={Model.APIKey}";
+
+        }
+        
+    }
+}
---
+++ WeatherForecastApp/Models/ProjectModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace WeatherForecastApp.Models
+{
+    public class ProjectModel
+    {
+        internal  readonly string APIKey = "9593624be1560fcf98d2b177f0c98dc8";
+
+        public  UriBuilder UriBuilder = new UriBuilder();
+    }
+}
